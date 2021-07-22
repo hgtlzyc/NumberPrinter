f108512a2b6c6c6d30449468372ccf5154fa301a
hgtlzyc <hgtlzycwk@gmail.com>
2021-07-22 00:49:49 -0400

    Update NumberPrinter.swift

--- NumberPrinter/NumberPrinter/Helper/NumberPrinter.swift
+++ NumberPrinter/NumberPrinter/Helper/NumberPrinter.swift
@@ -24,9 +24,7 @@ struct NumberPrinter {
             $0.append( strArrayFor($1).map{$0.rawValue} )
         }
         
-        let minLines = str2DArr.min{ $0.count > $1.count }?.count ?? 1
-        
-        return (0...minLines - 1).reduce(into: [String]() ) { sum, next in
+        return (0...4).reduce(into: [String]() ) { sum, next in
             var tempArr = [String]()
             str2DArr.forEach { strArray in
                 tempArr.append(strArray[next])
